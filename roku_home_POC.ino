e46a1b99284ba91aef5cf4241407341d019f4373
t.fish <tefisher@us.ibm.com>
2020-09-30 23:25:16 -0400

    Added sketches and readme

---
+++ roku_home_POC.ino
@@ -0,0 +1,63 @@
+#define AGC 9 //milliseconds
+#define AGC_PAUSE 4500 //microseconds
+#define PREDROP 625 ///microseconds
+#define ZERO 500 //microseconds
+#define ONE 1600 //microseconds
+
+#define OUTPIN 10
+
+int address = 0x5743;
+byte instruction = 0xC0;
+
+void ir_send(int address, byte instruction){
+  int i;
+  //send AGC preamble
+  digitalWrite(OUTPIN, LOW);
+  delay(AGC);
+  digitalWrite(OUTPIN, HIGH);
+  delayMicroseconds(AGC_PAUSE);
+  
+  //send address
+  for (i = 15; i >= 0; i--){
+    digitalWrite(OUTPIN, LOW);
+    delayMicroseconds(PREDROP);
+    digitalWrite(OUTPIN, HIGH);
+    if (bitRead(address,i)) delayMicroseconds(ONE);
+    else delayMicroseconds(ZERO);
+  }
+
+  //send instruction
+  for (i = 7; i >= 0; i--){
+    digitalWrite(OUTPIN, LOW);
+    delayMicroseconds(PREDROP);
+    digitalWrite(OUTPIN, HIGH);
+    if (bitRead(instruction,i)) delayMicroseconds(ONE);
+    else delayMicroseconds(ZERO);
+  }
+
+  //send inverse instruction
+   for (i = 7; i >= 0; i--){
+    digitalWrite(OUTPIN, LOW);
+    delayMicroseconds(PREDROP);
+    digitalWrite(OUTPIN, HIGH);
+    if (bitRead(instruction,i)) delayMicroseconds(ZERO);
+    else delayMicroseconds(ONE);
+  }
+  //Close Final Bit
+  digitalWrite(OUTPIN, LOW);
+  delayMicroseconds(PREDROP);
+  digitalWrite(OUTPIN, HIGH);
+}
+
+void setup() {
+  pinMode(OUTPIN, OUTPUT);
+  digitalWrite(OUTPIN, HIGH);
+}
+
+void loop() {
+  ir_send(address, instruction);
+  delay(40);
+  //second instruction - instruction + 1
+  ir_send(address, instruction + 1);
+  delay(1000);
+}
